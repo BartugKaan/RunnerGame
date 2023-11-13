85401229e7d103f5e0f3ae19c20efa5d3ba6d335
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2023-11-13 21:14:21 +0300

    +Added : Death animation and obstacles

--- RunnerGame/Assets/Scripts/PlayerMovement.cs
+++ RunnerGame/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 0.01f;
+    [SerializeField] float health = 100f;
     private Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,46 @@ public class PlayerMovement : MonoBehaviour
     {
         animator.SetBool("IsMoving", true);
         transform.Translate(Vector3.forward * speed);
+        HorizontalMovement();
+
     }
 
     void HorizontalMovement(){
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed);
+        if(transform.position.x > 1.2f){
+            transform.position = new Vector3(1.2f, transform.position.y, transform.position.z);            
+        }
+        else if(transform.position.x < -1.2f){
+            transform.position = new Vector3(-1.2f, transform.position.y, transform.position.z);            
+        }
+    }
+
+    void OnTriggerEnter(Collider collision){
+
+        Debug.Log("OnTriggerEnter called with " + collision.gameObject.tag);
+
+        if(collision.gameObject.tag == "Finish"){
+            animator.SetBool("IsMoving", false);
+            speed = 0;
+        }
+        if(collision.gameObject.tag == "Log Obstacle"){
+            health -= 30;
+            print(health);
+            if(health <= 0){
+                animator.SetBool("IsMoving", false);
+                animator.SetBool("IsDead", true);
+                speed = 0;
+            }
+        }
+        if(collision.gameObject.tag == "Rock Obstacle"){
+            health -= 50;
+            print(health);
+            if(health <= 0){
+                animator.SetBool("IsMoving", false);
+                animator.SetBool("IsDead", true);
+                speed = 0;
+            }
+        }
     }
 }
